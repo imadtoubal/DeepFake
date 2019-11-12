d094dbb3af084f285085e63ffe1a66c86bcde35a
calebheinzman <caleb.heinzman@gmail.com>
2019-11-11 23:05:16 -0600

    Updated to convert all frames to feature vectors and output csv file for the video.

--- MobileNet.py
+++ MobileNet.py
@@ -1,9 +1,9 @@
+import csv
 import cv2
 import numpy as np
 from PIL import Image
 from torchvision import transforms
 import torch
-import urllib
 import torch.nn as nn
 
 #Load the MobileNet Model
@@ -11,7 +11,10 @@ model = torch.hub.load('pytorch/vision:v0.4.2', 'mobilenet_v2', pretrained=True)
 model.eval()
 
 # File Name of VIdeo
-fileName = 'Data/sa1-video-fram1.avi'
+filePath = 'Data/'
+file = 'sa1-video-fram1'
+fileType = '.avi'
+fileName = filePath+file+fileType
 
 #Read Video
 cap = cv2.VideoCapture(fileName)
@@ -24,31 +27,42 @@ while success:
     # function extract frames
     success, image = cap.read()
     images.append(image)
-image = np.array(images[0])
-image = Image.fromarray(image)
 
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-input_tensor = preprocess(image)
-input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+frameFeatures = []
 
-# move the input and model to GPU for speed if available
-if torch.cuda.is_available():
-    input_batch = input_batch.to('cuda')
-    model.to('cuda')
+#Get Feature Vector for each frame
+for i in range(images.__len__()-1):
+    #Get the frame from list
+    image = np.array(images[i])
+    #Convert array to image type to work with preprocessing
+    image = Image.fromarray(image)
 
-#Removes Output Layer
-new_classifier = nn.Sequential(*list(model.classifier.children())[:-1])
-model.classifier = new_classifier
+    #Preprocessing to format and normalized the image
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(image)
+    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
 
-with torch.no_grad():
-    output = model(input_batch)
-# Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-print(output[0])
-# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-print(torch.nn.functional.softmax(output[0], dim=0))
+    # move the input and model to GPU for speed if available
+    if torch.cuda.is_available():
+        input_batch = input_batch.to('cuda')
+        model.to('cuda')
 
+    #Removes Output Layer
+    new_classifier = nn.Sequential(*list(model.classifier.children())[:-1])
+    model.classifier = new_classifier
+    #Gets Feature Vector
+    with torch.no_grad():
+        output = model(input_batch)
+
+    #Append Frame feature to list for entire video
+    frameFeatures.append(output[0].tolist())
+
+#Write Frame Features to Csv File
+with open(file+'.csv', "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(frameFeatures)
